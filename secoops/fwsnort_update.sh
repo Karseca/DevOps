de8db8534f18d77cda13cf78c34bcd8aa01f0ec0
s3c <s3cur3n3t@users.noreply.github.com>
2016-02-18 17:09:43 +0000

    Create fwsnort_update.sh
    This file uses the rm_tmp.conf file located in ./include folder, if you use install.sh located at the root of this repository it'll copy this script and the .conf file to it's final location: 
    
    --> script location: /usr/local/bin
    --> conf location: /usr/local/include

---
+++ secoops/fwsnort_update.sh
@@ -0,0 +1,36 @@
+#!/bin/bash                                     
+#                                                 
+#        fwsnort cron.daily update script         
+#             created by s3cur3n3t                
+#                 @216-02-2017                    
+#                                                 
+###################################################
+# Source conf file with the remove temp files
+source /usr/local/include
+
+# Global variables declaration
+snort_rules='https://rules.emergingthreats.net/open/'  
+check_snort_ver=`snort -V`
+work_tmp='/tmp' 
+rules_dir='/etc/snort/rules'
+snort_ver_file=$work_tmp/'snort_ver.txt' 
+sudo touch /usr/bin/fwsnort_update.sh
+
+# Prepare to donwload snort rules based on SNORT version
+echo "Checking snort version and prepare for rules download..."
+touch $snort_ver_file
+echo $check_snort_ver | grep    | awk '{ print }' >> $snort_ver_file
+wget https://$snort_rules/snort-$(cat $snort_ver_file)/rules/ -P /etc/snort/rules/
+echo "Firewall snort rules will starting..."
+touch $work_tmp/rules.txt
+ls -la $rules_dir/*.rules  >> $work_tmp/rules.txt
+cat $work_tmp/rules.txt | while read line do {
+                                               echo "Converting snort rules to iptables..."
+                                               fwsnort --snort-rfile /etc/snort/rules/$line
+                                               fwsnort
+                                               echo "Please wait, applying iptables rules..."
+                                               sudo /var/lib/fwsnort/fwsnort_iptcmds.sh                        
+                                              }
+                                          done
+# Remove temporary files created on upgrade process                                          
+rm_tmp 
